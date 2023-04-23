0414f71eb79ef4d0c242fb870ea3a96889666bab
Raga-M <ragavendran200@gmail.com>
2023-04-23 10:13:28 +0530

    thirukural

---
+++ thirukural/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Thirukkural from './component/thirukkural';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://www.tamilnadutourism.tn.gov.in/img/pages/medium-desktop/thiruvalluvar-statue-1656168684_5914195b5d4e0e9a7b75.webp" alt="thrivallur"/>
+   <Thirukkural/>
+    </div>
+  );
+}
+
+export default App;
---
+++ thirukural/src/component/thirukkural.js
@@ -0,0 +1,52 @@
+import React, {useState,useEffect} from "react"
+
+export default function Thirukkural(){
+    const [count,setCount]=useState(1)
+    const [kural,setKural]=useState(null)
+    function change(){
+        var k=Math.floor(Math.random()*1331)
+        if(k===0){
+        k++;}
+        setCount(k)
+      
+        }
+    
+  
+        useEffect(()=>{
+            console.log("render");
+        
+            async function fetchData(){
+                const response=await fetch(`https://api-thirukkural.vercel.app/api?num=${count}`).catch((err)=>console.log(err))
+                const res=await response.json();
+                setKural(res)}
+          
+          fetchData()
+        },[count])
+
+
+
+
+  
+
+  
+return(
+   <div className="container">
+<h1> திருக்குறள் </h1>
+{console.log(kural)}
+{/* <p>{kural.current}</p> */}
+<pre> குறள்:{kural?.number}      <span>பால்:{kural?.sect_tam}</span></pre>
+<p className="tamil">{kural?.line1}</p>
+<p className="tamil">{kural?.line2}</p>
+<h1>விளக்கம்</h1>
+<p className="explain tamil">{kural?.tam_exp}</p>
+
+<h1>English</h1>
+<pre> Number:{kural?.number}      <span>Section:{kural?.sect_eng}</span></pre>
+<p >{kural?.eng}</p>
+<h1>Explaination</h1>
+<p className="explain">{kural?.eng_exp}</p>
+
+
+<button className="btn" onClick={change}>அடுத்தது/Next</button>
+
+   </div> )}
